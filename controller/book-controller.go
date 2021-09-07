6f46a88b6491c01d44276a25ebe3923a36008e86
Adhi Ardiansyah <adhiardiansyah23@gmail.com>
2021-09-07 21:54:30 +0700

    book controller

---
+++ controller/book-controller.go
@@ -0,0 +1,141 @@
+package controller
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/adhiardiansyah/gin-gonic-restful-api/dto"
+	"github.com/adhiardiansyah/gin-gonic-restful-api/entity"
+	"github.com/adhiardiansyah/gin-gonic-restful-api/helper"
+	"github.com/adhiardiansyah/gin-gonic-restful-api/service"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+type BookController interface {
+	All(context *gin.Context)
+	FindByID(context *gin.Context)
+	Insert(context *gin.Context)
+	Update(context *gin.Context)
+	Delete(context *gin.Context)
+}
+
+type bookController struct {
+	bookService service.BookService
+	jwtService  service.JWTService
+}
+
+func NewBookController(bookService service.BookService, jwtService service.JWTService) BookController {
+	return &bookController{
+		bookService: bookService,
+		jwtService:  jwtService,
+	}
+}
+
+func (c *bookController) All(context *gin.Context) {
+	var books []entity.Book = c.bookService.All()
+	res := helper.BuildResponse(true, "OK!", books)
+	context.JSON(http.StatusOK, res)
+}
+
+func (c *bookController) FindByID(context *gin.Context) {
+	id, err := strconv.ParseUint(context.Param("id"), 0, 0)
+	if err != nil {
+		res := helper.BuildErrorResponse("Tidak ada parameter id yang ditemukan", err.Error(), helper.EmptyObj{})
+		context.AbortWithStatusJSON(http.StatusBadRequest, res)
+		return
+	}
+
+	var book entity.Book = c.bookService.FindByID(id)
+	if (book == entity.Book{}) {
+		res := helper.BuildErrorResponse("Data tidak ditemukan", "Tidak ada ada yang diberikan id", helper.EmptyObj{})
+		context.JSON(http.StatusNotFound, res)
+	} else {
+		res := helper.BuildResponse(true, "OK", book)
+		context.JSON(http.StatusOK, res)
+	}
+}
+
+func (c *bookController) Insert(context *gin.Context) {
+	var bookCreateDTO dto.BookCreateDTO
+	errDTO := context.ShouldBind(&bookCreateDTO)
+	if errDTO != nil {
+		res := helper.BuildErrorResponse("Gagal untuk memproses permintaan", errDTO.Error(), helper.EmptyObj{})
+		context.JSON(http.StatusBadRequest, res)
+	} else {
+		authHeader := context.GetHeader("Authorization")
+		userID := c.getUserIDByToken(authHeader)
+		convertedUserID, err := strconv.ParseUint(userID, 10, 64)
+		if err == nil {
+			bookCreateDTO.UserID = convertedUserID
+		}
+		result := c.bookService.Insert(bookCreateDTO)
+		response := helper.BuildResponse(true, "OK", result)
+		context.JSON(http.StatusOK, response)
+	}
+}
+
+func (c *bookController) Update(context *gin.Context) {
+	var bookUpdateDTO dto.BookUpdateDTO
+	errDTO := context.ShouldBind(&bookUpdateDTO)
+	if errDTO != nil {
+		res := helper.BuildErrorResponse("Gagal untuk memproses permintaan", errDTO.Error(), helper.EmptyObj{})
+		context.JSON(http.StatusBadRequest, res)
+		return
+	}
+
+	authHeader := context.GetHeader("Authorization")
+	token, errToken := c.jwtService.ValidateToken(authHeader)
+	if errToken != nil {
+		panic(errToken.Error())
+	}
+	claims := token.Claims.(jwt.MapClaims)
+	userID := fmt.Sprintf("%v", claims["user_id"])
+	if c.bookService.IsAllowedToEdit(userID, bookUpdateDTO.ID) {
+		id, errID := strconv.ParseUint(userID, 10, 64)
+		if errID == nil {
+			bookUpdateDTO.UserID = id
+		}
+		result := c.bookService.Update(bookUpdateDTO)
+		response := helper.BuildResponse(true, "OK", result)
+		context.JSON(http.StatusOK, response)
+	} else {
+		response := helper.BuildErrorResponse("Anda tidak diizinkan", "Anda tidak ownernya", helper.EmptyObj{})
+		context.JSON(http.StatusForbidden, response)
+	}
+}
+
+func (c *bookController) Delete(context *gin.Context) {
+	var book entity.Book
+	id, err := strconv.ParseUint(context.Param("id"), 0, 0)
+	if err != nil {
+		response := helper.BuildErrorResponse("Gagal untuk mendapatkan id", "Tidak ada id yang ditemukan", helper.EmptyObj{})
+		context.JSON(http.StatusBadRequest, response)
+	}
+	book.ID = id
+	authHeader := context.GetHeader("Authorization")
+	token, errToken := c.jwtService.ValidateToken(authHeader)
+	if errToken != nil {
+		panic(errToken.Error())
+	}
+	claims := token.Claims.(jwt.MapClaims)
+	userID := fmt.Sprintf("%v", claims["user_id"])
+	if c.bookService.IsAllowedToEdit(userID, book.ID) {
+		c.bookService.Delete(book)
+		response := helper.BuildResponse(true, "Terhapus", helper.EmptyObj{})
+		context.JSON(http.StatusOK, response)
+	} else {
+		response := helper.BuildErrorResponse("Anda tidak diizinkan", "Anda tidak ownernya", helper.EmptyObj{})
+		context.JSON(http.StatusForbidden, response)
+	}
+}
+
+func (c *bookController) getUserIDByToken(token string) string {
+	aToken, err := c.jwtService.ValidateToken(token)
+	if err != nil {
+		panic(err.Error())
+	}
+	claims := aToken.Claims.(jwt.MapClaims)
+	return fmt.Sprintf("%v", claims["user_id"])
+}
